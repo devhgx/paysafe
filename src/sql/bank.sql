5f4cfef9b7e63f9978b040a7d7ae42b53a6bf833
kridsadakorn.tip <kridsadakorntip@gosoft.co.th>
2023-07-19 13:37:36 +0700

    bank sql

---
+++ src/sql/bank.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.bank (bank_name,code,created_date,updated_date) VALUES
+	 ('KTB','ktb','2001-09-28 01:00:00',NULL),
+	 ('SCB','scb','2001-09-28 01:00:00',NULL),
+	 ('KBANK','kbank','2001-09-28 01:00:00',NULL),
+	 ('CIMB','cimb','2001-09-28 01:00:00',NULL);
